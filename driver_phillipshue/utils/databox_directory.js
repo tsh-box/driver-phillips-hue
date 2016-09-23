db0419fe3594e7127504679a3786c488b77d3a98
James Colley <james.colley@nottingham.ac.uk>
2016-09-23 14:59:57 +0100

    update:

---
+++ driver_phillipshue/utils/databox_directory.js
@@ -0,0 +1,40 @@
+
+var does_sensor_exist = function(){ // if local id exists for vendor then returns databox wide ID otherwise returns false
+
+}
+
+var is_vendor_registered = function(){ // if vendor name is registered then databox vendor id will be returned otherwise false; 
+
+} 
+
+exports.register_vendor = function() { // will return registered vendor id based on vendor name otherwise will attempt to register vendor
+
+}
+
+exports.register_sensor_type = function() {
+
+}
+
+exports.register_sensor = function() {
+
+}
+
+exports.register_actuator_type = function() {
+
+}
+
+exports.register_actuation_command = function() {
+
+}
+
+exports.register_actuator = function() {
+
+}
+
+exports.get_my_registered_sensor_types = function{ // takes in 
+
+}
+
+exports.get_my_registered_sensors = function{
+
+}
